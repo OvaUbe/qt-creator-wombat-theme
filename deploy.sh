f5cc0aaaeeeed002f760c44429b353a22c1a1148
Vladimir Golubev <vladimir.golubev@gs-labs.ru>
2018-03-28 13:34:32 +0300

    deploy: Verbose and force link creation.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ styles_path=`readlink -f ~/.config/QtProject/qtcreator/styles/`
 echo "Path to styles: ${styles_path}"
 
 mkdir "${styles_path}"
-ln -s ${PWD}/wombat-style.xml "${styles_path}/wombat-style.xml" 
+ln -vfs ${PWD}/wombat-style.xml "${styles_path}/wombat-style.xml" 
